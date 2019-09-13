44b1e405d73fc3e4a794676157d1089484b106d9
Nikke Leskelä <nileskel@gmail.com>
2019-09-13 15:56:17 +0300

    Update update-deps.sh

--- update-deps.sh
+++ update-deps.sh
@@ -5,37 +5,41 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $(realpath $SCRIPT_DIR)
 cd base
 
-echo "install dependencies"
-./yarn.sh install
-
-echo
-echo "compress node_modules"
-tar cjf node_modules.tar.bz2 node_modules/
-
-echo "upload to seperate github repo"
 CACHE_DIR=node_modules_cache
-CACHE_REPO=git@github.com:saavuio/s_node_2nd_cache.git
-VERSION=v1
 
-if [ -d $CACHE_DIR ]; then
-  rm -rf $CACHE_DIR
+if [ -z "$SKIP_BUILD" ]; then
+  echo "install dependencies"
+  ./yarn.sh install
+
+  echo
+  echo "compress node_modules"
+  tar cjf node_modules.tar.bz2 node_modules/
+
+  if [ -d $CACHE_DIR ]; then
+    rm -rf $CACHE_DIR
+  fi
+
+  mkdir $CACHE_DIR
+  mv node_modules.tar.bz2 $CACHE_DIR
 fi
 
-mkdir $CACHE_DIR
-mv node_modules.tar.bz2 $CACHE_DIR
+if [ ! -z "$PUSH_TO_REMOTE" ]; then
+  echo "upload to seperate github repo"
+  CACHE_REPO=git@github.com:saavuio/s_nuxt_2nd_cache.git
+  VERSION=v1
 
-# UNCOMMENT TO PUSH TO GITHUB
+  if [ ! -z "$1" ]; then
+    TARGET_BRANCH=main-repo-sha-$1
+  else
+    TARGET_BRANCH=$VERSION
+  fi
 
-if [ ! -z "$1" ]; then
-  TARGET_BRANCH=main-repo-sha-$1
-else
-  TARGET_BRANCH=$VERSION
+  rm -rf $CACHE_DIR/.git
+  cd $CACHE_DIR
+  git init
+  git remote add origin $CACHE_REPO
+  git checkout -b $TARGET_BRANCH
+  git add node_modules.tar.bz2
+  git commit -m "Latest."
+  git push -u -f origin $TARGET_BRANCH
 fi
-
-cd $CACHE_DIR
-git init
-git remote add origin $CACHE_REPO
-git checkout -b $TARGET_BRANCH
-git add node_modules.tar.bz2
-git commit -m "Latest."
-git push -u -f origin $TARGET_BRANCH
