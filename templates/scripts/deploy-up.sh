76c6bb3ba3f8f5ec5a19ad328d534d20b006c407
Nikke Leskelä <nileskel@gmail.com>
2019-09-13 15:53:23 +0300

    Update deploy-up.sh and tsconfig.json

--- templates/scripts/deploy-up.sh
+++ templates/scripts/deploy-up.sh
@@ -5,19 +5,28 @@ cd $SCRIPT_DIR
 cd ..
 
 ENVIRONMENT=$1
-ENV_FILE=env-$ENVIRONMENT
 ZEIT_TOKEN=$2
 ZEIT_TEAM=$3
-ZEIT_ENV=$(cat $ENV_FILE | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ -e /g')
+
+# NOTE:
+# Now v2 is having problems with typescript "paths" (see: https://github.com/zeit/now/issues/2832)
+# Converting each "@" to relative path.
+rm -rf src-to-deploy
+cp -a src/ src-to-deploy/
+
+A_FROM=" from '@/"
+find ./src-to-deploy -type f | while read LINE; do
+  RELDIR=$(realpath --relative-to=$(dirname $LINE) src-to-deploy)/
+  B_FROM=" from '$RELDIR"
+  sed -i'' "s!$A_FROM!$B_FROM!g" $LINE
+done
 
 # NOTE: recent change in now-cli now requires the "--target production" to
 # automatically follow the alias defined in now.json. So this isn't actually
 # going to production.
-
 ./scripts/cbin/now \
   -t $ZEIT_TOKEN \
-  $([ ! -z $ZEIT_TEAM ] && echo '-S $ZEIT_TEAM') \
-  -e $ZEIT_ENV \
+  $([ ! -z $ZEIT_TEAM ] && echo "-S $ZEIT_TEAM") \
   -A now.$ENVIRONMENT.json \
   --target production \
   deploy
